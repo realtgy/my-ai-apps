f5a89096e8f663ffa30ed1a0b3ce7ef3ec50c52a
realtgy <donaldjones2895@gmail.com>
2025-11-24 19:24:13 +0800

    feat: connect server and client

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -6,6 +6,12 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.get("/api/hello", (req: Request, res: Response) => {
+  res.json({
+    message: "Hello world!",
+  });
+});
+
 app.get("/", (req: Request, res: Response) => {
   res.send(`OPENAI_AI_KEY:${process.env.OPENAI_API_KEY}`);
 });

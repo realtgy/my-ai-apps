97d0b9db2d75492780d9ff2c8dd8bc01f64cad75
realtgy <donaldjones2895@gmail.com>
2025-11-29 14:59:55 +0800

    feat: add react-loading-skeleton for loading states and implement StarRating component for review ratings

--- packages/client/src/components/reviews/ReviewList.tsx
+++ packages/client/src/components/reviews/ReviewList.tsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import StarRating from './StarRating';
+import Skeleton from 'react-loading-skeleton';
 
 type ReviwListProps = {
    productId: string;
@@ -20,21 +22,42 @@ type GetReviewsResponse = {
 
 const ReviewList = ({ productId }: ReviwListProps) => {
    const [reviewData, setReviewData] = useState<GetReviewsResponse>();
+   const [isLoading, setIsLoading] = useState(true);
+
    useEffect(() => {
       const fetchReviews = async () => {
+         setIsLoading(true);
          const { data } = await axios.get<GetReviewsResponse>(
             `/api/products/${productId}/reviews`
          );
          setReviewData(data);
+         setIsLoading(false);
       };
       fetchReviews();
    }, [productId]);
+
+   if (isLoading) {
+      return (
+         <div className="flex flex-col gap-5">
+            {[1, 2, 3].map((p) => (
+               <div key={p}>
+                  <Skeleton width={150} />
+                  <Skeleton width={100} />
+                  <Skeleton count={2} />
+               </div>
+            ))}
+         </div>
+      );
+   }
+
    return (
       <div className="flex flex-col gap-5">
          {reviewData?.reviews.map((review) => (
             <div key={review.id}>
                <div className="font-semibold">{review.author}</div>
-               <div>Rating: {review.rating}/5</div>
+               <div>
+                  Rating: <StarRating value={review.rating} />
+               </div>
                <div className="py-2">{review.content}</div>
             </div>
          ))}

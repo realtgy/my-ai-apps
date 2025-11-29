cb9a56c76aa9763aa6d27306b5b534f889e505cf
realtgy <donaldjones2895@gmail.com>
2025-11-29 15:14:56 +0800

    refactor: remove unused useState and useEffect imports in ReviewList component

--- packages/client/src/components/reviews/ReviewList.tsx
+++ packages/client/src/components/reviews/ReviewList.tsx
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { useState, useEffect } from 'react';
 import StarRating from './StarRating';
 import Skeleton from 'react-loading-skeleton';
 import { useQuery } from '@tanstack/react-query';
